9e1aa66534ebf186841a00cebbb2de47b904d229
leoarthurwall <leowall93@gmail.com>
2022-09-16 12:53:58 +0100

    added javascript to change color when tempurature increases of decreses past certain points. code is working

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import "./index.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(10);
-  const [color, setColor] = useState("hot");
+  const [count, setCount] = useState(-5);
+  const [color, setColor] = useState("freezing");
 
   const Increase = () => {
-    const newTemp = count + 1
+    const newTemp = count + 1;
     setCount(newTemp);
+
+    if (newTemp >= 40) {
+      setColor("roasting");
+    } else if (newTemp >= 20) {
+      setColor("hot");
+    } else if (newTemp >= 10) {
+      setColor("warm");
+    } else if (newTemp > 0) {
+      setColor("cold");
+    } else {
+      setColor("freezing");
+    }
   };
   const Decrease = () => {
-    const newTemp = count - 1
+    const newTemp = count - 1;
     setCount(newTemp);
+
+    if (newTemp <= 0) {
+      setColor("freezing");
+    } else if (newTemp <= 10) {
+      setColor("cold");
+    } else if (newTemp <= 20) {
+      setColor("warm");
+    } else if (newTemp <= 40) {
+      setColor("hot");
+    } else {
+      setColor("roasting");
+    }
   };
 
   return (
