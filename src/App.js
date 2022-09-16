349627e963b1baa6b210f7b11cec7e40239a255b
leoarthurwall <leowall93@gmail.com>
2022-09-16 12:22:12 +0100

    added onClick event handlers for increase and decreasing the tempurature. temp now goes up and down 1 degree per click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './index.css';
+import "./index.css";
+import {useState} from 'react'
 
 function App() {
+    const[count, setCount] = useState(10)
+
+    const Increase = () => {
+      setCount(count + 1)
+    }
+    const Decrease = () => {
+      setCount(count - 1)
+    }
+
+
   return (
     <div className="App-Container">
-      <div className='Temp-Container'>
-        <div className='Temp-Display'>10°C</div>
+      <div className="Temp-Container">
+        <div className="Temp-Display">{count}°C</div>
       </div>
-      <div className='Button-Container'>
-        <button>+</button>
-        <button>-</button>
+      <div className="Button-Container">
+        <button onClick={Increase}>+</button>
+        <button onClick={Decrease}>-</button>
       </div>
     </div>
   );
